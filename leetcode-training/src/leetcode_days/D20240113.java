cc9154835d74a10f99f0408b4f6bb21717a0de23
gaopeiyuan <121370451@qq.com>
2024-01-14 03:24:00 +0800

    2024-1-13 完善

--- leetcode-training/src/leetcode_days/D20240113.java
+++ leetcode-training/src/leetcode_days/D20240113.java
@@ -44,92 +44,98 @@ import java.util.Map;
  * <p>
  * 1 <= repeatLimit <= s.length <= 105
  * s 由小写英文字母组成
+ * 自己的解法超出时间限制
+ * 最差的时候的时间复杂度为o(n^2)
  */
 public class D20240113 {
-//    public char[] quickSort(char[] s, int left, int right) {
-//        if (left < right) {
-//            int partition = partition(s, left, right);
-//            quickSort(s, left, partition - 1);
-//            quickSort(s, partition + 1, right);
-//        }
-//        return s;
-//    }
-//
-//    private int partition(char[] charArray, int left, int right) {
-//        int pivot = charArray[left];
-//        while (left < right) {
-//            while (left < right && (int) charArray[right] < pivot) right--;
-//            if (left < right) charArray[left++] = charArray[right];
-//            while (left < right && (int) charArray[left] > pivot) left++;
-//            if (left < right) charArray[right--] = charArray[left];
-//        }
-//        charArray[left] = (char) pivot;
-//        return left;
-//    }
-//
-//    public boolean swapCharArray(char[] arr, char pre, int preIndex) {
-//        for (int tempIndex = preIndex + 1; tempIndex < arr.length && arr[tempIndex] < pre; tempIndex++) {
-//            char temp = arr[preIndex];
-//            arr[preIndex] = arr[tempIndex];
-//            arr[tempIndex] = temp;
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    public String repeatLimitedString(String s, int repeatLimit) {
-//        char[] chars = quickSort(s.toCharArray(), 0, s.length() - 1);
-//        if (s.length() == 1) return s;
-//        int count = 1;
-//        char pre = chars[0];
-//        for (int index = 1; index < chars.length; index++) {
-//            if (chars[index] == pre) {
-//                count++;
-//            } else {
-//                pre = chars[index];
-//                count = 1;
-//            }
-//            if (count > repeatLimit) {
-//                if (!swapCharArray(chars, pre, index)) {
-//                    return String.valueOf(Arrays.copyOfRange(chars, 0, index));
-//                }
-//                count = 1;
-//                index++;
-//            }
-//        }
-//        return String.valueOf(chars);
-//    }
-static public int N = 26;
-    public String repeatLimitedString(String s, int repeatLimit) {
-        int[] count = new int[N];
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
+    public char[] quickSort(char[] s, int left, int right) {
+        if (left < right) {
+            int partition = partition(s, left, right);
+            quickSort(s, left, partition - 1);
+            quickSort(s, partition + 1, right);
         }
-        StringBuilder ret = new StringBuilder();
-        int m = 0;
-        for (int i = N - 1, j = N - 2; i >= 0 && j >= 0;) {
-            if (count[i] == 0) { // 当前字符已经填完，填入后面的字符，重置 m
-                m = 0;
-                i--;
-            } else if (m < repeatLimit) { // 当前字符未超过限制
-                count[i]--;
-                ret.append((char)('a' + i));
-                m++;
-            } else if (j >= i || count[j] == 0) { // 当前字符已经超过限制，查找可填入的其他字符
-                j--;
-            } else { // 当前字符已经超过限制，填入其他字符，并且重置 m
-                count[j]--;
-                ret.append((char)('a' + j));
-                m = 0;
+        return s;
+    }
+
+    private int partition(char[] charArray, int left, int right) {
+        int pivot = charArray[left];
+        while (left < right) {
+            while (left < right && (int) charArray[right] < pivot) right--;
+            if (left < right) charArray[left++] = charArray[right];
+            while (left < right && (int) charArray[left] > pivot) left++;
+            if (left < right) charArray[right--] = charArray[left];
+        }
+        charArray[left] = (char) pivot;
+        return left;
+    }
+
+    public boolean swapCharArray(char[] arr, char pre, int preIndex) {
+        for (int tempIndex = preIndex + 1; tempIndex < arr.length; tempIndex++) {
+            if (arr[tempIndex] < pre){
+                char temp = arr[preIndex];
+                arr[preIndex] = arr[tempIndex];
+                arr[tempIndex] = temp;
+                return true;
             }
         }
-        return ret.toString();
+        return false;
     }
+
+    public String repeatLimitedString(String s, int repeatLimit) {
+        char[] chars = quickSort(s.toCharArray(), 0, s.length() - 1);
+        if (s.length() == 1) return s;
+        int count = 1;
+        char pre = chars[0];
+        for (int index = 1; index < chars.length; index++) {
+            if (chars[index] == pre) {
+                count++;
+            } else {
+                pre = chars[index];
+                count = 1;
+            }
+            if (count > repeatLimit) {
+                if (!swapCharArray(chars, pre, index)) {
+                    return String.valueOf(Arrays.copyOfRange(chars, 0, index));
+                }
+                count = 1;
+                index++;
+            }
+        }
+        return String.valueOf(chars);
+    }
+//static public int N = 26;
+//    public String repeatLimitedString(String s, int repeatLimit) {
+//        int[] count = new int[N];
+//        for (int i = 0; i < s.length(); i++) {
+//            count[s.charAt(i) - 'a']++;
+//        }
+//        StringBuilder ret = new StringBuilder();
+//        int m = 0;
+//        for (int i = N - 1, j = N - 2; i >= 0 && j >= 0;) {
+//            if (count[i] == 0) { // 当前字符已经填完，填入后面的字符，重置 m
+//                m = 0;
+//                i--;
+//            } else if (m < repeatLimit) { // 当前字符未超过限制
+//                count[i]--;
+//                ret.append((char)('a' + i));
+//                m++;
+//            } else if (j >= i || count[j] == 0) { // 当前字符已经超过限制，查找可填入的其他字符
+//                j--;
+//            } else { // 当前字符已经超过限制，填入其他字符，并且重置 m
+//                count[j]--;
+//                ret.append((char)('a' + j));
+//                m = 0;
+//            }
+//        }
+//        return ret.toString();
+//    }
 
     public static void main(String[] args) {
         D20240113 d = new D20240113();
         //yxxrrrrqppliddd
         //yxxrrrrqppliddd
-        System.out.printf(d.repeatLimitedString("pdprlxqryxdirdr", 10));
+        //yxxvvuvusrrqqpp
+        //yxxvvuvusrrqqppopponliihgfeeddcbba
+        System.out.printf(d.repeatLimitedString("robnsdvpuxbapuqgopqvxdrchivlifeepy", 2));
     }
 }
